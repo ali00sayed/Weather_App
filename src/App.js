5be9dc50327a90b3b1fc2dd432f6b6ceb11e4014
ali00sayed <ali00sayed@outlook.com>
2022-02-12 14:15:18 +0530

    Wheather_App

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Weather from "./components/weather/Weather";
+import Footer from "./components/footer/Footer";
+const key = "d0b49537880c4a4193357cc5e92210a0";
+
+export default function App() {
+  const [weatherData, setweatherData] = useState([]);
+  const [city, setCity] = useState("");
+
+  //fetching data from weathbit.io api
+  async function getLocationWeather(city) {
+
+    const result = await fetch(
+      `https://api.weatherbit.io/v2.0/current?city=${city}&key=${key}&include=minutely`
+    );
+
+    const res = await result.json();
+    setweatherData(res.data);
+
+  }
+
+  //getting value from input
+  const updateInput = (evt) => {
+    setCity(evt.target.value);
+  };
+
+  //changing bacground based on time
+
+  useEffect(() => {
+    getLocationWeather("Chittorgarh");
+  }, []);
+
+  return (
+    <div className="main">
+      <div className="navbar">
+        <h1 className="title">Mausam</h1>
+      </div>
+      <div className="searchBar">
+        <input
+          className="searchBlock"
+          placeholder="Search City"
+          onChange={updateInput}
+          required />
+        <button
+          className="searchBtn"
+          onClick={() => {
+            getLocationWeather(city);
+          }}
+        >
+          Search
+        </button>
+      </div>
+
+      {weatherData.length > 0 ? (
+        <>
+          <Weather weather={weatherData[0]} />
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+      <Footer />
+    </div>
+  );
+}
+
+//
